e3e837f5e7578dcb47944e1d02646cb0f6268ccc
Channaka Gunawardhane <channakagunawardana@gmail.com>
2024-06-22 11:13:53 +0530

    login route

--- server/database.py
+++ server/database.py
@@ -1,6 +1,14 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+
 DATABASE_URL='postgresql://postgres:admin@localhost:5432/flutter_musicapp'
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine )
-db= SessionLocal()
+
+def get_db():
+    db=SessionLocal()
+    try:
+        yield db
+    finally:db.close()
+    
--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,14 +1,17 @@
 import uuid
 import bcrypt
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 
+from database import get_db
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
+from pydantic_schemas.user_login import UserLogin
+from sqlalchemy.orm import Session
 
 router=APIRouter()
 
-@router.post('/signup')
-def signup_user(user:UserCreate):
+@router.post('/signup',status_code=201)
+def signup_user(user:UserCreate, db: Session=Depends(get_db)):
     #check if the user already exist in db
     user_db=db.query(User).filter(User.email == user.email).first()
     
@@ -23,4 +26,19 @@ def signup_user(user:UserCreate):
     db.commit()
     db.refresh(user_db)
     
+    
     return user_db
+
+@router.post('/login')
+def login_user(user:UserLogin, db:Session=Depends(get_db)):
+    #check if a user exist 
+    user_db= db.query(User).filter(User.email==user.email).first()
+    
+    if not user_db:
+        raise HTTPException(400,'User with this email does not exits')
+    #password check
+    is_match=bcrypt.checkpw(user.password.encode(),user_db.password)
+    if not is_match:
+        raise HTTPException(400,'Incorrect Password')
+    return user_db
+        
